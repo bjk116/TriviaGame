58d834677afaed77da57e9c6be12b17d42665808
Brian K <karabinchak.brian@gmail.com>
2017-04-25 19:47:47 -0400

    Added background images, some styling, 2 of 10 question bank

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var questionBank = {
+	1: ['What classic film was called production 9401 during filming?', 'Psycho', 'Back Window', 'Strangers on a Train', 'Casablana', 'pic of Psycho'],
+	2: ['What color were the slippers in the original Wizard of Oz?', 'Silver', 'Gold', 'White', 'Yellow', 'pic of slippers'],
+	3: 
+	4:
+	5:
+	6:
+	7:
+	8:
+	9:
+	10:
+}
