4823f76f066765d553914244b912d7739a2df2e1
Brian K <karabinchak.brian@gmail.com>
2017-04-27 09:48:59 -0400

    Finished question bank

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,52 @@
 var questionBank = {
-	1: ['What classic film was called production 9401 during filming?', 'Psycho', 'Back Window', 'Strangers on a Train', 'Casablana', 'pic of Psycho'],
-	2: ['What color were the slippers in the original Wizard of Oz?', 'Silver', 'Gold', 'White', 'Yellow', 'pic of slippers'],
-	3: 
-	4:
-	5:
-	6:
-	7:
-	8:
-	9:
-	10:
+	1: 	{
+		'question': 'What classic film was called production 9401 during filming?',
+		'answers':['Psycho', 'Back Window', 'Strangers on a Train', 'Casablana'],
+		'picUrl': 'pic of Psycho',
+		},
+	2: 	{
+		'question':'What color were the slippers in the original Wizard of Oz?',
+		'answers':['Silver', 'Gold', 'White', 'Yellow'],
+		'picUrl': 'pic of slippers'
+		},
+	3: {
+		'question':'In the first two Jaws movies, what was the police chiefs name?',
+		'answers':['Martin Brody', 'John Ash', 'Carl Lennyson', 'Clark Kent'],
+		'picUrl': 'pic of slippers'
+		},
+	4: {
+		'question':'Raquel Welch once had a job as what?',
+		'answers':['Weathergirl', 'Waitress', 'Taxi Driver', 'Full Stack Developer'],
+		'picUrl': 'pic of slippers'
+		},
+	5: {
+		'question':'In the Terminator, Sarah Connar was played by who?',
+		'answers':['Linda Hamilton', 'Sigourney Weaver', 'Angelina Jolie', 'Meiko Kaji'],
+		'picUrl': 'pic of slippers'
+		},
+	6: {
+		'question':'What screenwriter has recieved the most Oscar nominations?',
+		'answers':['Woody Allen', 'Ron Howard', 'David Fincher', 'Stephen King'],
+		'picUrl': 'pic of slippers'
+		},
+	7: {
+		'question':'What is Sean Connery\'s real first name?',
+		'answers':['Thomas', 'George', 'James', 'Peter'],
+		'picUrl': 'pic of slippers'
+		},
+	8: {
+		'question':'Alfred Hitchcock admitted to being afraid of what?',
+		'answers':['Policeman', 'Spiders', 'World War', 'Being cliche'],
+		'picUrl': 'pic of slippers'
+		},
+	9: {
+		'question':'Which actor starred in the Sixth Sense?',
+		'answers':['Bruce Willis', 'Dolph Lundgren', 'Charlie Day', 'Glenn Howerton'],
+		'picUrl': 'pic of slippers'
+		},
+	10: {
+		'question':'Which actor has been portrayed most on screen by other actors?',
+		'answers':['Charlie Chaplin', 'Burt Reynolds', 'Sean Connery', 'William Shatner'],
+		'picUrl': 'pic of slippers'
+		},
 }
